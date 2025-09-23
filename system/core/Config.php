71f3104969e3afbe955de146b1094f97b68592bf
MichalSkoula <michal@skoula.cz>
2025-09-23 20:55:04 +0200

    config->item() can return array

--- system/core/Config.php
+++ system/core/Config.php
@@ -195,7 +195,7 @@ class CI_Config {
 	 *
 	 * @param	string	$item	Config item name
 	 * @param	string	$index	Index name
-	 * @return	string|null	The configuration item or NULL if the item doesn't exist
+	 * @return	string|array|null	The configuration item or NULL if the item doesn't exist
 	 */
 	public function item($item, $index = '')
 	{
